e14b86b546a58001c4a3509258fe6552583f1551
Bart Scheer <bscheer078@gmail.com>
2026-01-16 18:12:30 +0000

    GitHub Copilot Prompting

---
+++ basic_prompt.js
@@ -0,0 +1,11 @@
+function convertToCamelCase(str) {
+    return str
+        .split(/[\s_]+/)
+        .map((word, index) => {
+            if (index === 0) {
+                return word.toLowerCase();
+            }
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+        })
+        .join('');
+}
